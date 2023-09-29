0a17cc0386d2eebb8e7a5ba6305f3c3c6357f5c9
unknown <shishkina.ja@gmail.com>
2023-09-29 20:45:12 +0200

    feat: add local storage usage

--- random-game/script.js
+++ random-game/script.js
@@ -5,6 +5,7 @@ let firstCard, secondCard
 let firstAttr, secondAttr
 let lockGameTable = false
 let countClick = 0
+let isFinished
 
 function reverseCard() {
   if(lockGameTable) return;
@@ -68,4 +69,107 @@ function rundomiseCards() {
     card.style.order = randomPos
   })
 }
-rundomiseCards()
+// rundomiseCards()
+
+
+cards.forEach(card => card.addEventListener('click', checkAllCardsReversed))
+
+function checkAllCardsReversed(){
+  setTimeout(()=> {
+    let arrLength = document.querySelectorAll('.reverse').length
+    console.log(document.querySelectorAll('.reverse'));
+    if(arrLength === 16) {
+      console.log('ok');
+      currentGamer.score = countClick
+      console.log(currentGamer);
+      /* add gamer to scoreList */
+      scoreList.unshift(currentGamer)
+      /* render actual scoreTable */
+      updLocalStorage()
+      clearRenderedList()
+      updRenderedList()
+      addCover()
+    } else {
+      console.log(`${arrLength}`);
+    }
+  }, 300)
+  
+}
+
+function addCover() {
+  document.querySelector('.game-cards-cover').classList.add('game-cards-cover_active')
+}
+function removeCover(){
+  document.querySelector('.game-cards-cover').classList.remove('game-cards-cover_active')
+}
+
+let renderedList = document.querySelector('.list')
+function clearRenderedList(){
+  renderedList.innerHTML = ''
+}
+
+function updRenderedList(){
+  scoreList.map(gamer => {
+    let listItem = `<li class="text">${gamer.name} - ${gamer.score}</li>`
+    renderedList.insertAdjacentHTML('beforeend', listItem)
+  })
+} 
+/* remove reverse class all cards */
+
+function resetGame() {
+  countClick = 0
+  cards.forEach(card => {
+    card.classList.remove('reverse');
+    card.addEventListener('click', reverseCard);
+  });
+}
+
+/* local storage */
+let currentGamer
+let scoreList = []
+/* load local storage */
+window.addEventListener('load', () => {
+  let storedGamers = JSON.parse(localStorage.getItem('scoreList'))
+  if (storedGamers.length > 10) {
+    storedGamers.pop()
+  }
+  console.log(storedGamers);
+  if(storedGamers) {
+    scoreList = storedGamers
+    clearRenderedList()
+    updRenderedList()
+  }
+})
+addCover()
+/* upd local st */
+function updLocalStorage(){
+  localStorage.setItem('scoreList', JSON.stringify(scoreList));
+}
+class gamerScore {
+  constructor(name){
+    this.name = name;
+    this.score = 0;
+  }
+}
+
+
+/* new game */
+const newGameBtn = document.querySelector('.new-game-btn')
+newGameBtn.addEventListener('click', (e)=> {
+  e.preventDefault()
+  // document.location.reload()
+  resetGame()
+  rundomiseCards()
+  removeCover()
+  lockGameTable = false
+  // new user
+  
+  let currName = document.querySelector('.input-name').value
+  if(currName) {
+    currentGamer = new gamerScore(currName)
+    console.log(currentGamer);
+    return currentGamer
+  }
+  return currentGamer
+})
+/* localStorage.clear() */
